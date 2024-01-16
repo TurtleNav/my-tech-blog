c5cc28efffd4db9b74b3e2d49e30a25df8c73cd2
TurtleNav <tourangeau_luc@yahoo.com>
2024-01-16 01:17:37 -0500

    Bare minimum setup is complete with homepage view

---
+++ controllers/api/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+
+//router.use('/recipe', recipeRoutes);
+//router.use('/users', userRoutes);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now Listening'));
+});
