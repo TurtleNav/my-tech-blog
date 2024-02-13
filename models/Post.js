f3d7d7a3169185dc708f8de91399903e3270abea
TurtleNav <tourangeau_luc@yahoo.com>
2024-02-13 17:06:49 -0500

    built out post model, next up is a comment model

---
+++ models/Post.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    post_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    creator_id: {
+      // user that created this post
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id"
+      }
+    },
+    comment_id: {
+      // comments on post
+      type: DataTypes.INTEGER,
+      references: {
+        model: "comment",
+        key: "id"
+      }
+
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = User;
